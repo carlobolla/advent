d917da63ee5a04266b9649f089af64b6b2878e15
Carlo Bollini <14152206+carlobolla@users.noreply.github.com>
2022-12-04 21:21:42 +0100

    Was drunk and used an enum...

--- AdventOfCode/Solutions/Year2022/Day03/Day03.cs
+++ AdventOfCode/Solutions/Year2022/Day03/Day03.cs
@@ -7,10 +7,7 @@ namespace AdventOfCode.Solutions.Year2022;
 
 internal class Day03 : ASolution
 {
-    private enum Priority
-    {
-        a = 1,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
-    }
+    private readonly char[] _priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
     private readonly List<string> _lines;
 
     public Day03() : base(03, 2022, "Rucksack Reorganization")
@@ -20,14 +17,14 @@ internal class Day03 : ASolution
 
     protected override string SolvePartOne()
     {
-        return _lines.Sum(line => (int)Enum.Parse<Priority>(line[..(line.Length / 2)].First(x => line[(line.Length / 2)..].Contains(x)).ToString())).ToString();
+        return _lines.Sum(line => Array.IndexOf(_priority, line[..(line.Length / 2)].First(x => line[(line.Length / 2)..].Contains(x))) +1).ToString();
     }
 
     protected override string SolvePartTwo()
     {
         var groups = _lines.GroupBy(x => _lines.IndexOf(x) / 3);
         int prioritySum = groups.Select(group => group.ToArray())
-            .Select(array => (int)Enum.Parse<Priority>(array[0].Intersect(array[1]).Intersect(array[2]).First().ToString()))
+            .Select(array => Array.IndexOf(_priority, array[0].Intersect(array[1]).Intersect(array[2]).First()) +1)
             .Sum();
 
         return prioritySum.ToString();
