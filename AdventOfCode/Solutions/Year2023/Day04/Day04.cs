bba628c1043fc40e488f32e8bcdc58533489589d
Carlo Bollini <carlo.bollini@reti.it>
2023-12-06 16:58:30 +0100

    Day04 and Day05 part 1

---
+++ AdventOfCode/Solutions/Year2023/Day04/Day04.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions.Year2023
+{
+    internal class Day04 : ASolution
+    {
+        private readonly string[] lines;
+        //this also captures garbage in position 0. I can't be bothered to fix it, so i'll just skip position 0 later
+        private readonly Regex numberRegex = new("([ ][ 1-9][0-9])");
+        private readonly Regex cardIdRegex = new(@"Card *([\d]*):");
+
+        public Day04() : base(04, 2023, "Scratchcards")
+        {
+            lines = Input.SplitByNewline();
+        }
+
+        private class Card
+        {
+            public int Id { get; init; }
+            public List<int> WinningNumbers { get; init; } = new();
+            public List<int> CardNumbers { get; init; } = new();
+        }
+
+        private static int getScore(Card card, bool summed)
+        {
+            var winning = card.WinningNumbers.Intersect(card.CardNumbers).Count();
+            if(summed)
+                return winning == 0 ? 0 : (int)Math.Pow(2, winning - 1);
+            return winning;
+        }
+
+        private List<Card> GetCards()
+        {
+            return lines.Select(l =>
+            {
+                var matches = numberRegex.Matches(l);
+                return new Card()
+                {
+                    Id = int.Parse(cardIdRegex.Match(l).Groups[1].Value),
+                    CardNumbers = matches.Skip(11).Select(m => int.Parse(m.Value)).ToList(),
+                    WinningNumbers = matches.Skip(1).Take(10).Select(m => int.Parse(m.Value)).ToList(),
+                };
+            }).ToList();
+        }
+        
+        protected override string SolvePartOne()
+        {
+            return GetCards()
+                .Sum(c => getScore(c, true))
+                .ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            var cards = GetCards();
+            var counters = Enumerable.Range(1, cards.Count).ToDictionary(i => i, _ => 1);
+            cards.ForEach(c =>
+            {
+                foreach (var i in Enumerable.Range(c.Id, getScore(c, false)))
+                {
+                    counters[i+1] += counters[c.Id];
+                }
+            });
+            return counters.Values.Sum().ToString();
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Year2023/Day05/Day05.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions.Year2023
+{
+    internal class Day05 : ASolution
+    {
+        private readonly List<string> lines;
+        private const string SEED_TO_SOIL = "seed-to-soil map:";
+        private const string SOIL_TO_FERTILIZER = "soil-to-fertilizer map:";
+        private const string FERTILIZER_TO_WATER = "fertilizer-to-water map:";
+        private const string WATER_TO_LIGHT = "water-to-light map:";
+        private const string LIGHT_TO_TEMPERATURE = "light-to-temperature map:";
+        private const string TEMPERATURE_TO_HUMIDITY = "temperature-to-humidity map:";
+        private const string HUMIDITY_TO_LOCATION = "humidity-to-location map:";
+        
+        public Day05() : base(05, 2023, "If You Give A Seed A Fertilizer")
+        {
+            lines = Input.SplitByNewline().ToList();
+        }
+
+        private record Range(long destination, long source, long rangeLength);
+
+        private List<Range> getRanges(string from, string to)
+        {
+            var a = lines.SkipWhile(l => l != from).TakeWhile(l => l != to).Skip(1).Select(r =>
+            {
+                var splits = r.Split(" ").Select(long.Parse).ToList();
+                return new Range(splits[0], splits[1], splits[2]);
+            }).ToList();
+            return a;
+        }
+
+        private List<long> getSeeds()
+        {
+            return Regex.Matches(lines[0], @" (\d*)").Select(m => long.Parse(m.Value)).ToList();
+        }
+
+        private static long ConvertIfInRange(long test, Range range)
+        {
+            return test < (range.source + range.rangeLength) && test >= range.source
+                ? test + range.destination - range.source
+                : -1;
+        }
+        
+        protected override string SolvePartOne()
+        {
+            var seeds = getSeeds();
+            var soil = seeds.Select(s =>
+                getRanges(SEED_TO_SOIL, SOIL_TO_FERTILIZER).Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            var fertilizer = soil.Select(s =>
+                getRanges(SOIL_TO_FERTILIZER, FERTILIZER_TO_WATER).Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            var water = fertilizer.Select(s =>
+                getRanges(FERTILIZER_TO_WATER, WATER_TO_LIGHT).Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            var light = water.Select(s =>
+                getRanges(WATER_TO_LIGHT, LIGHT_TO_TEMPERATURE).Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            var temperature = light.Select(s =>
+                getRanges(LIGHT_TO_TEMPERATURE, TEMPERATURE_TO_HUMIDITY).Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            var humidity = temperature.Select(s =>
+                getRanges(TEMPERATURE_TO_HUMIDITY, HUMIDITY_TO_LOCATION).Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            var location = humidity.Select(s =>
+                getRanges(HUMIDITY_TO_LOCATION, "").Select(r => ConvertIfInRange(s, r))
+                    .FirstOrDefault(d => d != -1, s));
+            return location.Min().ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return "";
+        }
+    }
+}
